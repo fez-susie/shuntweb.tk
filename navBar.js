5b7168cb61051bd3e7fe4995123e006c96429e64
fez-susie <aid.desousa@stjohns.sa.edu.au>
2022-04-29 10:25:01 +0930

    Upload to GitHub

---
+++ navBar.js
@@ -0,0 +1,8 @@
+function navi() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
